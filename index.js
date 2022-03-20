a5839133b286c3ce8957af8fd46299d546078180
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-03-20 17:52:07 +0100

    :rocket: Isolate Resolvers

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./graphql/typeDefs.js";
-import { db } from "./firebase.js";
-
-// import { resolvers } from "./graphql/resolvers/index.js";
-import { collection, onSnapshot } from "firebase/firestore";
-import { users } from "./graphql/resolvers/users.js";
-// import { posts } from "./graphql/resolvers/posts.js";
-
-console.log(users);
-// Get collection with specified Reference
-// const colRef = collection(db, "user");
-
-// let users = [];
-
-// Real time collection data
-// const unsubCol = onSnapshot(colRef, (snapshot) => {
-//   users = [];
-//   snapshot.docs.forEach((doc) => {
-//     users.push({ id: doc.id, ...doc.data() });
-//   });
-//   console.log(users);
-//   // return Users;
-// });
-
-const resolvers = {
-  Query: {
-    Users: () => users,
-    // Posts: () => posts,
-  },
-};
+import { resolvers } from "./graphql/resolvers/resolvers.js";
 
 // console.log(resolvers);
 const server = new ApolloServer({ typeDefs, resolvers });
