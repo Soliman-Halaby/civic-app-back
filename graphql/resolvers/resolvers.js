7016902c149d53ccbe2d917c7fce27a9b4f221f1
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-03-22 00:34:10 +0100

    :construction:  Add users mutation

--- graphql/resolvers/resolvers.js
+++ graphql/resolvers/resolvers.js
@@ -1,13 +1,41 @@
 import { users } from "./users.js";
 import { posts } from "./posts.js";
 // import { dateScalar } from "../typeDefs.js";
+// import { Mutation } from "./users.js";
+let links = [
+  {
+    id: "link-0",
+    email: "solimanalhalabygmailcom",
+  },
+];
+
+console.log(links);
 const resolvers = {
   // Date: dateScalar,
   Query: {
     Users: () => users,
     Posts: () => posts,
+    Feed: () => links,
   },
+  // Mutation: {
+  //   ...Mutation,
+  // },
   // Date: dateScalar,
+  Mutation: {
+    // 2
+    register: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        email: args.email,
+      };
+      // console.log("cc");
+      links.push(link);
+      console.log(link);
+      return link;
+    },
+  },
 };
 
 // export { resolvers };
