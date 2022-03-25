e09efe4043dd6a0b94837665edafe43ba4da3347
Rob1 <robin.bourachot@hetic.net>
2022-03-25 02:03:56 +0100

    :fire:  Remove query

--- graphql/resolvers/Queries/index.js
+++ graphql/resolvers/Queries/index.js
@@ -1,8 +1,7 @@
 export * from "./getUser.js";
 // export * from "./getUsers.js";
 export * from "./getPosts.js";
-export * from "./getPostsApi.js";
 // export * from "./getUserPosts.js";
-// export * from "./getUserId.js";
+export * from "./getUserById.js";
 
 // console.log(getPosts);
